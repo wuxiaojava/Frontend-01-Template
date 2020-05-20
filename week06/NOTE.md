f295a2621461868e8e8fa613fb145fd9efb99582
wuxiaojava <fujinjava@126.com>
2020-05-20 21:14:30 +0800

    ok

--- week06/NOTE.md
+++ week06/NOTE.md
@@ -1,1 +1,4 @@
 # 每周总结可以写在这里
+
+解析html的状态机状态：
+tagOpen -> tagName ->  beforeAttributeName/selfClosingStartTag/tagName
---
+++ week06/TOY-BROWER/parser.js
@@ -0,0 +1,436 @@
+const css = require("css");
+const EOF = Symbol("EOF");//end of file 
+
+let currentToken = null;
+let currentAttribute = null;
+let stack = [{type:"document",children:[]}];
+let currentTextNode = null;
+
+//加入一个新的函数 addCSSRules,这里我们把css规则暂存到一个数组里
+let rules = [];
+
+function addCSSRules(text){
+    var ast = css.parse(text);
+    console.log(JSON.stringify(ast,null,"    "));
+    rules.push(...ast.stylesheet.rules);
+    console.log(rules);
+}
+
+function match(element,selector){
+    if(!selector || !element.attributes){
+        return false;
+    }
+
+    if(selector.charAt(0) == "#"){
+        var attr = element.attributes.filter(attr => attr.name === "id")[0];
+        if(attr && attr.value === selector.replace("#",'')){
+            return true;
+        }
+    }else if(selector.charAt(0) == "."){
+        var attr = element.attributes.filter(attr => attr.name === "class")[0];
+        if (attr && attr.value === selector.replace(".", '')) {
+            return true;
+        }
+    }else{
+        if(element.tagName === selector){
+            return true;
+        }
+    }
+}
+
+function specificity(selector){
+    var p = [0,0,0,0];
+    var selectorParts = selector.split(" ");
+    for(var part of selectorParts){
+        if(part.charAt(0) == "#"){
+            p[1] += 1;
+        }else if(part.charAt(0) == "."){
+            p[2] += 1;
+        }else{
+            p[3] += 1;
+        }
+    }
+
+    return p;
+}
+
+function computeCSS(element){
+    var elements = stack.slice().reverse();
+    if(!element.computeStyle){
+        element.computeStyle = {};
+    }
+
+    console.log(rules.length);
+    for(let rule of rules){
+        var selectorParts = rule.selectors[0].split(" ").reverse();
+        if(!match(element,selectorParts[0])){
+            continue;
+        }
+
+        var j = 1;
+        for(var i = 0; i<elements.length; i++){
+            if(match(elements[i],selectorParts[j])){
+                j++;
+            }
+        }
+
+        if(j >= selectorParts.length){
+            matched = true;
+        }
+
+        if(matched){
+            var sp = specificity(rule.selectors[0]);
+            var computeStyle = element.computeStyle;
+
+            for(var declaration of rule.declarations){
+                if(!computeStyle[declaration.property]){
+                    computeStyle[declaration.property] = {};
+                }
+                if (computeStyle[declaration.property].specificity){
+                    computeStyle[declaration.property].value = declaration.value;
+                    computeStyle[declaration.property].specificity = sp;
+                } else if (compare(computeStyle[declaration.property].specificity,sp) < 0){
+                    computeStyle[declaration.property].value = declaration.value;
+                    computeStyle[declaration.property].specificity = sp;
+                }
+            }
+        }
+    }
+}
+
+function compare(sp1,sp2){
+    if(sp1[0] - sp2[0]){
+        return sp1[0] - sp2[0];
+    }
+    if (sp1[1] - sp2[1]) {
+        return sp1[1] - sp2[1];
+    }
+    if (sp1[2] - sp2[2]) {
+        return sp1[2] - sp2[2];
+    }
+
+    return sp1[3] - sp2[3];
+}
+
+function emit(token){
+    if(token.type != "text"){   
+        console.log(currentToken);
+    }
+    let top = stack[stack.length - 1];
+    
+    if(token.type == "startTag"){
+        let element = {
+            type:"element",
+            children:[],
+            attributes:[]
+        };
+
+        element.tagName = token.tagName;
+
+        for(let p in token){
+            if(p != "type" || p != "tagName"){
+                element.attributes.push({
+                    name:p,
+                    value:token[p]
+                });
+            }
+        }
+        computeCSS(element);
+        top.children.push(element);
+
+        if(!token.isSelfClosing){
+            stack.push(element);
+        }
+
+        currentTextNode = null;
+    }else if(token.type == "endTag"){ 
+        if(top.tagName != token.tagName){
+            console.log("Tag start and doesn't match!");
+            //throw new Error("Tag start and doesn't match!");
+        }else{
+            //遇到style标签时，执行添加Css规则的操作
+            if(top.tagName == "style"){
+                addCSSRules(top.children[0].content);
+            }
+            stack.pop();
+        }
+        currentTextNode = null;
+    } else if (token.type == "text") {
+        if(currentTextNode == null){
+            currentTextNode = {
+                type:"text",
+                content:""
+            }
+            top.children.push(currentTextNode);
+        }
+        currentTextNode.content += token.content;
+    }
+    
+}
+
+
+//标签开始状态
+function tagOpen(c){
+    if(c == "/"){
+        return endTagOpen;
+    }else if(c.match(/^[a-zA-Z]$/)){
+        currentToken = {
+            type:"startTag",
+            tagName:""
+        }
+        return tagName(c);
+    }else{
+        emit({
+            type:"text",
+            content:c
+        });
+        return;
+    }
+}
+
+function tagName(c){
+    if(c.match(/^[\t\n\f ]$/)){
+        return beforeAttributeName;
+    }else if(c == "/"){
+        return selfClosingStartTag;
+    }else if(c.match(/^[a-zA-Z]$/)){
+        currentToken.tagName += c;
+        return tagName;
+    }else if(c == '>'){
+        emit(currentToken);
+        return data;
+    }else{
+        currentToken.tagName += c;
+        return tagName;
+    }
+}
+
+function beforeAttributeName(c){
+    if(c.match(/^[\t\n\f ]$/)){
+        return beforeAttributeName;
+    }else if(c == "/" || c == ">" || c == EOF){
+        return afterAttributeName;
+    }else if(c == "="){
+
+    }else{
+        currentAttribute = {
+            name: "",
+            value:""
+        }
+        return attributeName(c);
+    }
+}
+
+function attributeName(c){
+    if (c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+        return afterAttributeName(c);
+    }else if(c == "="){
+        return beforeAttributeValue;
+    }else if(c == "\u0000"){
+
+    }else if(c == "\"" || c == "'" || c == "<"){
+
+    }else{
+        currentAttribute.name += c;
+        return attributeName;
+    }
+}
+
+function beforeAttributeValue(c){
+    if (c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+        return beforeAttributeValue;
+    }else if(c == "\""){
+        return doubleQuotedAttributeValue;
+    }else if(c == "\'"){
+        return singleQuotedAttributeValue;
+    }else if(c == ">"){
+        return data;
+    }else{
+        return UnquotedAttributeValue(c);
+    }
+}
+
+function doubleQuotedAttributeValue(c){
+    if(c == "\""){
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    }else if(c == "\u0000"){
+
+    }else if(c == EOF){
+
+    }else{
+        currentAttribute.value += c;
+        return doubleQuotedAttributeValue;
+    }
+}
+
+function singleQuotedAttributeValue(c){
+    if (c == "\"") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    } else if (c == "\u0000") {
+
+    } else if (c == EOF) {
+
+    } else {
+        currentAttribute.value += c;
+        return doubleQuotedAttributeValue;
+    }
+}
+
+function afterQuotedAttributeValue(c){
+    if(c.match(/^[\t\n\f ]$/)){
+        return beforeAttributeName;
+    } else if (c == "/") {
+        return selfClosingStartTag;
+    } else if (c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else {
+        currentAttribute.value += c;
+        return doubleQuotedAttributeValue;
+    }
+}
+
+function UnquotedAttributeValue(c){
+    if(c.match(/^[\t\n\f ]$/)){
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return beforeAttributeName;
+    } else if (c == "/") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return selfClosingStartTag;
+    } else if (c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (c == "\u0000") {
+
+    } else if (c == "\"" ||c == "'" ||c == "<" || c == "=" || c == "`" ){
+
+    } else if(c == EOF){
+       
+    } else{
+         currentAttribute.value += c;
+         return UnquotedAttributeValue;
+    }
+}
+
+function selfClosingStartTag(c){
+    if(c == ">"){
+        currentToken.isSelfClosing = true;
+        emit(currentToken);
+        return data;
+    }else if(c == EOF){
+        
+    }else{
+
+    }
+}
+
+function endTagOpen(c){
+    if(c.match(/^[a-zA-Z]$/)){
+        currentToken = {
+            type:"endTag",
+            tagName:""
+        }
+        return tagName(c);
+    }else if(c == ">"){
+
+    }else if(c == EOF){
+
+    }else{
+
+    }
+}
+
+function afterAttributeName(c){
+    if(c.match(/^[\t\n\f ]$/)){
+        return afterAttributeName;
+    }else if(c == "/"){
+        return selfClosingStartTag;
+    }else if(c == "="){
+        return beforeAttributeValue;
+    }else if(c == ">"){
+        currentToken[currentAttribute.attributeName] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    }else if(c == EOF){
+        
+    }else{
+        currentToken[currentAttribute.attributeName] = currentAttribute.value;
+        currentAttribute = {
+            name:"",
+            value:""
+        };
+        return attributeName(c);
+    }
+}
+
+
+function data(c) {
+    if (c == "<") {
+        return tagOpen;
+    } else if(c == "<"){
+        return endTagOpen;
+    } else if (c == EOF) {
+        emit({
+            type: "EOF"
+        });
+        return;
+    } else {
+        emit({
+            type: "text",
+            content: c
+        });
+        return data;
+    }
+}
+
+// module.exports.parseHTML = 
+
+// function parseHTML(html){
+//     for(let c of html){
+//         //获取状态
+//         state = data(c);
+//     }
+
+//     state = data(EOF);
+//     return stack[0];
+// }
+
+var html = `<html a=b>
+    <head>
+    <style>
+    body div# myid {
+        width: 100 px;
+        background-color: #ff5000;
+    }
+    body div img {
+        width: 30px;
+        background-color: #ff1111;
+    } 
+
+    </style> 
+    </head>
+    <body>
+        <div>
+            <img id = "myDiv" />
+            <img />
+        </div> 
+    </body>
+</html>
+`;
+
+
+function parseHTML(html){
+    let state = data;
+    for(let c of html){
+        state = state(c);
+    }
+
+    state = state(EOF);
+    return stack[0];
+}
+
+parseHTML(html);
