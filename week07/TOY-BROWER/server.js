0b4a31077486afc63586c1df4d4b73b8045e86ee
wuxiaojava <fujinjava@126.com>
2020-05-26 14:18:22 +0800

    优化

--- week07/TOY-BROWER/server.js
+++ week07/TOY-BROWER/server.js
@@ -1,7 +1,6 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
-    console.log(req);
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('X-Foo', 'bar');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
